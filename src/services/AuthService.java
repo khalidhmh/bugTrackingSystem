7f0cdb0eb68491d91ac71b6bc8e177c1fc5adfa3
Eng. Khalid Hussain <212400902@fci.helwan.edu.eg>
2025-12-17 22:13:05 +0200

    AuthServices
    Implemented login method for user authentication.

--- src/services/AuthService.java
+++ src/services/AuthService.java
@@ -4,9 +4,30 @@ package services;
  * Authentication service for login/logout
  * @author Team
  */
+
+import dao.UserDAO;
+import models.User;
+
 public class AuthService {
-    
-    // TODO: Implement login method
-    // TODO: Implement logout method
-    // TODO: Implement password validation
+
+    private final UserDAO userDAO;
+
+    public AuthService() {
+        this.userDAO = new UserDAO();
+    }
+
+    public User login(String username, String password) {
+
+        if (username == null || password == null) {
+            return null;
+        }
+
+        User user = userDAO.findByUsername(username.trim());
+
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+
+        return null; // login failed
+    }
 }
